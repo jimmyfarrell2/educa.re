49921bf523b77355e78b5f6bfcf03e4a682f8cda
Sonia Trehan <soniatrehan6@gmail.com>
2015-05-07 15:14:44 -0400

    our first meta commit - Sonia, Nastia, Jimmy, David

--- server/app/routes/document/index.js
+++ server/app/routes/document/index.js
@@ -6,25 +6,32 @@ var git = Promise.promisifyAll(require('gift'));
 var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
 
+var repo = null;
+
+//set a repo for the user
+router.use(function(req, res, next){
+    req.userPath = path.join(__dirname, '/../../../../documents/' + req.body.id);
+    req.docPath = path.join(req.userPath, '/' + req.body.name);
+    req.repo = Promise.promisifyAll(git(req.docPath));
+    next();
+});
 
 
 
 //create client's first folder
 router.post('/', function(req, res, next){
 
-    var documentPath = path.join(__dirname, '/../../../../documents/' + req.body.id);
-
-    mkdirp(documentPath)
+    mkdirp(req.userPath)
         .then(function(){
-            mkdirp(documentPath + '/' + req.body.name);
+            mkdirp(req.docPath);
         })
         .then(function(){
-            fs.writeFileAsync(documentPath + '/' + req.body.name + '/' + req.body.document + '.md');
+            fs.writeFileAsync(req.docPath + '/contents.md', 'welcome');
         })
         .then(function(){
-            return git.initAsync(__dirname + '/../../../../documents/' + req.body.id  + '/' +  req.body.name);
+            return git.initAsync(req.docPath);
         })
-        .then(function(repo){
+        .then(function(_repo){
             res.send(200);
         })
         .catch(function(err){
@@ -32,5 +39,30 @@ router.post('/', function(req, res, next){
         });
 
 });
+
+//update a user's file and commit
+router.put('/', function(req, res, next){
+
+
+    fs.writeFileAsync(req.docPath + '/contents.md', 'NOT anything else')
+        .then(function(){
+            return req.repo.addAsync('contents.md');
+        })
+        .then(function(){
+            return req.repo.commitAsync('our first commit');
+        })
+        .then(function(){
+            return req.repo.commitsAsync();
+        })
+        .then(function(commits){
+            console.log(commits);
+        })
+        .catch(function(err){
+            return next(err);
+        });
+
+
+});
+
 
 module.exports = router;
