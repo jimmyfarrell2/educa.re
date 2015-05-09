c4cadef2abf522fc36059b2548a3cf5760df0d53
Jimmy Farrell <jfarrell22@gmail.com>
2015-05-09 12:29:01 -0400

    Clone logic and route created, create repo abstracted, signup now creates user folder, all of us

--- server/app/routes/document/index.js
+++ server/app/routes/document/index.js
@@ -11,7 +11,6 @@ var router = require('express').Router(),
 //set a repo for the user
 router.use('/', function(req, res, next){
     req.userPath = path.join(__dirname, '/../../../../documents/' + req.user._id);
-    req.docPath = path.join(req.userPath, '/' + req.body.document.title);
     req.repo = Promise.promisifyAll(git(req.docPath));
     next();
 });
@@ -20,24 +19,9 @@ router.use('/', function(req, res, next){
 //create client's first folder
 router.post('/', function(req, res, next){
 
-    var currentVersion = '';
     mkdirp(req.userPath)
-        .then(function(){
-            mkdirp(req.docPath);
-        })
-        .then(function(){
-            return fs.writeFileAsync(req.docPath + '/contents.md', 'welcome');
-        })
-        .then(function(_currentVersion){
-            currentVersion = _currentVersion;
-            return git.initAsync(req.docPath);
-        })
-        .then(function(){
-            req.body.document.currentVersion = currentVersion;
-            return Document.createAsync(req.body.document);
-        })
-        .then(function(doc){
-            return User.findByIdAndUpdateAsync(req.user._id, {$push: {'documents': doc._id}});
+        .then(function() {
+            return createRepo(req);
         })
         .then(function(){
             res.send(200);
@@ -71,5 +55,57 @@ router.put('/', function(req, res, next){
 
 });
 
+//clone a repo into another user's account and commit
+router.post('/clone', function(req, res, next) {
+
+    req.body.document.author = req.user._id;
+    req.body.document.readAccess = [];
+    req.body.document.editAccess = [];
+    req.body.pathToRepo = '';
+    var doc;
+    Document.createAsync(req.body.document)
+        .then(function(_doc) {
+            doc = _doc;
+            doc.pathToRepo = path(req.userPath, '/' + doc._id);
+            return doc.saveAsync();
+        })
+        .then(function() {
+            return git.cloneAsync(req.body.document.pathToRepo, doc.pathToRepo);
+        })
+        .then(function() {
+            res.sendStatus(200);
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+
+});
+
+function createRepo(request) {
+
+    var doc;
+    var docPath = '';
+
+    return Document.createAsync(request.body.document)
+        .then(function(_doc) {
+            doc = _doc;
+            return User.findByIdAndUpdateAsync(request.user._id, {$push: {'documents': doc._id}});
+        })
+        .then(function() {
+            docPath = path.join(request.userPath, '/' + doc._id);
+            return mkdirp(docPath);
+        })
+        .then(function(){
+            return fs.writeFileAsync(docPath + '/contents.md', doc.currentVersion);
+        })
+        .then(function(){
+            return git.initAsync(docPath);
+        })
+        .then(function() {
+            doc.pathToRepo = docPath;
+            return doc.saveAsync();
+        });
+
+}
 
 module.exports = router;
