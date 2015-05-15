05fba0d5bdf20919872af8175a68549c0231e393
Sonia Trehan <soniatrehan6@gmail.com>
2015-05-15 11:49:31 -0400

    versioning and converting to md working

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,15 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
     });
+});
+
+app.controller('HomeCtrl', function($scope, $state, DocumentFactory){
+    $scope.createDocument = function(){
+        DocumentFactory.createDocument().then(function(doc){
+            $state.go('editor', {docId: doc._id});
+        });
+    }
 });
