1ed914add3836ebf67fdaad389506a350be9b996
NastiaS <anastasiasergienko@gmail.com>
2015-05-15 17:42:57 -0400

    Modals for commit history start Nastia | Sonia

--- browser/js/documentDashboard/documentDashboard.js
+++ browser/js/documentDashboard/documentDashboard.js
@@ -20,7 +20,7 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('DocumentDashboardController', function($scope, DocumentFactory, user, document, commits){
+app.controller('DocumentDashboardController', function($scope, $modal, DocumentFactory, user, document, commits){
     $scope.commits = commits;
     $scope.document = document;
     $scope.user = user;
@@ -32,5 +32,51 @@ app.controller('DocumentDashboardController', function($scope, DocumentFactory, 
 
   $scope.alertMe = function() {
     
+  };
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+});
+
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
   };
 });
