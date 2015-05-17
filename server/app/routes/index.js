de23a77b353e38e47d32337a68289fa8db272440
Jimmy Farrell <jfarrell22@gmail.com>
2015-05-17 19:46:08 -0400

    Functionality for searching documents and users is complete, David and Jimmy

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -12,6 +12,8 @@ router.use('/collaborate', require('./collaborate'));
 
 router.use('/user', require('./user'));
 
+router.use('/search', require('./search'));
+
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
