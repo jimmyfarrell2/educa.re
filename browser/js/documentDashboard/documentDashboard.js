63fcbc2bab649333b63d2761ad8633d521e050bf
NastiaS <anastasiasergienko@gmail.com>
2015-05-22 20:47:41 -0400

    Cleaned up and refactored modals in documentDashboard

--- browser/js/documentDashboard/documentDashboard.js
+++ browser/js/documentDashboard/documentDashboard.js
@@ -22,15 +22,19 @@ app.config(function ($stateProvider) {
 
 app.controller('DocumentDashboardController', function($scope, $log, $modal, DocumentFactory, user, document, commits, $state, $window){
 
+    $scope.document = document;
+    $scope.user = user;
+
     $scope.commits = commits.filter(function(commit) {
       return commit.authored_date >= document.dateCreated;
     });
 
+//This method returns a modal instance controlled by VersionModalInstanceCtrl with a specified version of a doc
     $scope.showVersion = function(commit, document) {
         var modalInstance = $modal.open({
             animation: true,
-            templateUrl: 'versionModalContent.html',
-            controller: 'VersionModalCtrl',
+            templateUrl: 'versionModal',
+            controller: 'VersionModalInstanceCtrl',
             size: 'lg',
             resolve: {
                 versionContent: function(){
@@ -49,80 +53,37 @@ app.controller('DocumentDashboardController', function($scope, $log, $modal, Doc
         });
     };
 
-    $scope.document = document;
-    $scope.user = user;
+//This method returns a modal instance controlled by PullReqModalInstanceCtrl with a specified pull request 
+    $scope.showPullReqs = function (size, document, index, pullRequest) {
 
-  $scope.tabs = [
-    { title:'Dynamic Title 1', content:'Dynamic content 1' },
-    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-  ];
-
-  $scope.alertMe = function() {
-
-  };
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size, document, index, pullRequest) {
-
-    var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        content: function(){
-            var converter = $window.markdownit({
-                html: true
-            });
-            return converter.render(pullRequest.proposedVersion);
-        },
-        document: function(){
-            return document;
-        },
-        index: function(){
-            return index;
-        },
-        pullRequest: function(){
-            return pullRequest;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    });
-  };
-
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-});
-
-
-app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, content, $state, document, index, pullRequest, $sce) {
-    $scope.content = $sce.trustAsHtml(content);
-    $scope.index = index;
-    $scope.pullRequest = pullRequest;
-
-  $scope.mergeChanges = function(){
-    $state.go('editor', {docId:document._id, pullReq: index});
-    $scope.ok();
+        var modalInstance = $modal.open({
+            templateUrl: 'pullReqModal',
+            controller: 'PullReqModalInstanceCtrl',
+            size: size,
+            resolve: {
+                content: function(){
+                    var converter = $window.markdownit({
+                        html: true
+                    });
+                    return converter.render(pullRequest.proposedVersion);
+                },
+                document: function(){
+                    return document;
+                },
+                index: function(){
+                    return index;
+                },
+                pullRequest: function(){
+                    return pullRequest;
+                }
+            }
+        });
     };
-
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
 });
 
-app.controller('VersionModalCtrl', function($scope, $modalInstance, versionContent, $state, $sce, $window, DocumentFactory, document, commit) {
-
+//A modal instance with a specified version of a doc
+app.controller('VersionModalInstanceCtrl', function($scope, $modalInstance, versionContent, $state, $sce, $window, DocumentFactory, document, commit) {
+    
     $scope.differentContent = versionContent !== document.currentVersion;
 
     var converter = $window.markdownit({
@@ -145,10 +106,32 @@ app.controller('VersionModalCtrl', function($scope, $modalInstance, versionConte
     };
 
     $scope.ok = function () {
-        $modalInstance.close($scope.selected);
+        $modalInstance.close();
     };
 
     $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
+        $modalInstance.dismiss();
     };
 });
+
+//A modal instance with a specified pull request
+app.controller('PullReqModalInstanceCtrl', function ($scope, $modalInstance, content, $state, document, index, pullRequest, $sce) {
+    $scope.content = $sce.trustAsHtml(content);
+    $scope.index = index;
+    $scope.pullRequest = pullRequest;
+
+    $scope.mergeChanges = function(){
+        $state.go('editor', {docId:document._id, pullReq: index});
+        $scope.ok();
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+});
+
+
